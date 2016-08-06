17b3a20636206df24b046d7ef0ee60e8f12fede8
Tinashe Itayi <Tinashe Itayi>
2016-08-06 13:52:19 +1200

    Add project files and folders

---
+++ helloPerson.ts
@@ -0,0 +1,7 @@
+function greeter(person: string) {
+    return "Hello, " + person;
+}
+
+var user = "What is Hello";
+
+document.body.innerHTML = greeter(user);  
---
+++ musichandler.ts
@@ -0,0 +1,8 @@
+var myClientId = "d759c157a0c5fbb632c11f3e3f89ed7c";
+
+function initSC() : void {
+    // init SoundCloud
+    SC.initialize({
+        client_id: myClientId
+    });
+}
